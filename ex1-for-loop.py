485acff6b025aefc85fcc95819c3ce38063513f4
Ankit Nath <ankitnath1993@gmail.com>
2024-03-24 23:02:20 +0100

    2nd commit: modified both .py files

---
+++ ex1-for-loop.py
@@ -0,0 +1,12 @@
+#count to n and print the number
+n = int(input("Enter the value of n: "))
+def count_to_n(x=n):
+    for i in range(1,x+1):
+        print(i,end='')
+    print()
+
+print("Going to count to {} ...".format(n))
+count_to_n(n)
+
+#---------------------------------------------------
+print("first modification for 2nd commit")
---
+++ ex2-function.py
@@ -0,0 +1,7 @@
+def double(n):
+    return 2*n   #return twice the given number
+#Call the function with the value of your choice and print its results
+print("Call the function and print the value")
+print("The program ran successfully")
+#--------------------------------------------------
+print("first modification for 2nd commit")
